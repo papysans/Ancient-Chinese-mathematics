42eecebd74c9f972588c6bd44a8b420ae8739c76
Napstablook <807399089@qq.com>
2024-03-19 20:56:21 +0800

    Refactor playGIF.js: Update event listeners and add image preloading

--- src/tools/Abacus/playGIF.js
+++ src/tools/Abacus/playGIF.js
@@ -1,16 +1,20 @@
 // 获取 gif 图片元素
-var gif = document.getElementById('myGif');
+var gif = document.getElementById("myGif");
 
 // 设置 gif 图片的静态版本和动态版本的 url
-var staticSrc = 'path/to/static.gif';
-var animatedSrc = 'path/to/animated.gif';
+var staticSrc = "path/to/static.gif";
+var animatedSrc = "path/to/animated.gif";
+
+// 预加载动态 gif
+var preloadImage = new Image();
+preloadImage.src = animatedSrc;
 
 // 监听鼠标 hover 事件
-gif.addEventListener('mouseover', function() {
+gif.addEventListener("mouseover", function () {
     gif.src = animatedSrc; // 播放 gif
 });
 
 // 监听鼠标移出事件
-gif.addEventListener('mouseout', function() {
+gif.addEventListener("mouseout", function () {
     gif.src = staticSrc; // 停止播放 gif
 });
